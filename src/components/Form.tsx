4a33acca98000a7c6226952e066e47f787398edd
richfjiq <rfjiq1986@gmail.com>
2024-03-03 18:56:22 -0600

    chore: adds formik to handle form submission

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,87 @@
+import { useFormik } from 'formik';
+import { useState } from 'react';
+import * as Yup from 'yup';
+import { mutate } from 'swr';
+
+import { todosApi } from '../api';
+import Loading from './Loading';
+
+const todosUrl = 'http://localhost:8080/api/todos';
+
 const Form = () => {
+  const [loading, setLoading] = useState(false);
+  const { handleSubmit, errors, touched, getFieldProps, resetForm } = useFormik(
+    {
+      initialValues: {
+        title: '',
+        description: '',
+      },
+      onSubmit: async (values) => {
+        try {
+          setLoading(true);
+          await todosApi.post('', {
+            ...values,
+            complete: false,
+          });
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+          resetForm();
+          mutate(todosUrl);
+        }
+      },
+      validationSchema: Yup.object({
+        title: Yup.string()
+          .min(5, 'Must have at least 5 characters.')
+          .required('Required'),
+        description: Yup.string()
+          .min(10, 'Must have at least 10 characters.')
+          .required('Required'),
+      }),
+    }
+  );
+
   return (
     <div className="w-full px-5 md:px-0 md:w-[75%] py-10">
-      <div className="flex flex-col">
-        <label className="mb-2">Title</label>
-        <input
-          className="border rounded px-2 py-2 mb-8 border-[#1f2544e6]"
-          placeholder="Add a title"
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="mb-2">Description</label>
-        <input
-          className="border rounded px-2 py-2 mb-8 border-[#1f2544e6]"
-          placeholder="Add description"
-        />
-      </div>
-      <button className="bg-[#1f2544] hover:bg-[#1f2544e6] text-white px-8 py-2 rounded">
-        Add todo
-      </button>
+      <form onSubmit={handleSubmit} noValidate>
+        <div className="flex flex-col relative">
+          <label htmlFor="title" className="mb-2">
+            Title
+          </label>
+          <input
+            type="text"
+            className="border rounded px-2 py-2 mb-8 border-[#1f2544e6]"
+            placeholder="Add a title"
+            {...getFieldProps('title')}
+          />
+          {touched.title && errors.title && (
+            <span className="absolute bottom-2 text-sm text-[#B31312]">
+              {errors.title}
+            </span>
+          )}
+        </div>
+        <div className="flex flex-col relative">
+          <label className="mb-2">Description</label>
+          <input
+            type="text"
+            className="border rounded px-2 py-2 mb-8 border-[#1f2544e6]"
+            placeholder="Add description"
+            {...getFieldProps('description')}
+          />
+          {touched.description && errors.description && (
+            <span className="absolute bottom-2 text-sm text-[#B31312]">
+              {errors.description}
+            </span>
+          )}
+        </div>
+        <button
+          type="submit"
+          className="bg-[#1f2544] hover:bg-[#1f2544e6] text-white w-[130px] h-[40px] flex justify-center items-center rounded"
+        >
+          {loading ? <Loading /> : 'Add todo'}
+        </button>
+      </form>
     </div>
   );
 };
